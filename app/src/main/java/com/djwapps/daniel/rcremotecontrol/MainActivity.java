f94b382d4639a53fc11e90bf7938344184d778ee
DanWilliams24 <01danielwilliams@gmail.com>
2019-06-19 19:37:36 -0400

    Replaced OnClick Listeners with OnTouch listeners to facilitate real-time constant communications with RC Host, rather than the previous setup which only sent a singular message when a button was pressed.

--- app/src/main/java/com/djwapps/daniel/rcremotecontrol/MainActivity.java
+++ app/src/main/java/com/djwapps/daniel/rcremotecontrol/MainActivity.java
@@ -1,18 +1,11 @@
 package com.djwapps.daniel.rcremotecontrol;
 
 import android.content.DialogInterface;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.icu.util.Output;
-import android.os.Build;
-import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,18 +14,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
     private ImageButton startButton;
-    private Button settingsButton,leftButton,rightButton,upButton,downButton,stopButton;
+    private Button settingsButton,leftButton,rightButton,upButton,downButton;
     private TextView ipView;
     private String ip;
     private Boolean networkerEstablished = false;
     private Networker networker;
     private Toast notificationBanner;
+
     public enum COMMAND {
         LEFT,RIGHT,UP,DOWN,STOP
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +41,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         upButton = findViewById(R.id.upButton);
         downButton = findViewById(R.id.downButton);
         ipView = findViewById(R.id.ipView);
-        stopButton = findViewById(R.id.stopButton);
         ip = "255.255.255.255";
         startButton.setOnClickListener(this);
         settingsButton.setOnClickListener(this);
-        leftButton.setOnClickListener(this);
-        rightButton.setOnClickListener(this);
-        upButton.setOnClickListener(this);
-        downButton.setOnClickListener(this);
-        stopButton.setOnClickListener(this);
+
+        leftButton.setOnTouchListener(this);
+        rightButton.setOnTouchListener(this);
+        upButton.setOnTouchListener(this);
+        downButton.setOnTouchListener(this);
         notificationBanner = Toast.makeText(getApplicationContext(),"This device is not yet connected to the RC",Toast.LENGTH_SHORT);
 
 
@@ -64,6 +59,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.ip = ip;
         ipView.setText(ip);
 
+    }
+
+
+
+    private boolean onTouchDownCondition(View v, MotionEvent e, int id ){
+        return (networkerEstablished && (v.getId() == id) && (e.getAction() == MotionEvent.ACTION_DOWN));
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if(onTouchDownCondition(v,event,R.id.leftButton)){
+            Log.d("RC-UI", "LEFT Button Pressed");
+            attemptToSend("" + COMMAND.LEFT.name().charAt(0));
+
+        }else if(onTouchDownCondition(v,event,R.id.rightButton)){
+            Log.d("RC-UI", "RIGHT Button Pressed");
+            attemptToSend("" + COMMAND.RIGHT.name().charAt(0));
+
+        }else if(onTouchDownCondition(v,event,R.id.upButton)){
+            Log.d("RC-UI", "UP Button Pressed");
+            attemptToSend("" + COMMAND.UP.name().charAt(0));
+
+        }else if(onTouchDownCondition(v,event,R.id.downButton)){
+            Log.d("RC-UI", "DOWN Button Pressed");
+            attemptToSend("" + COMMAND.DOWN.name().charAt(0));
+        }
+
+
+        if(networkerEstablished && event.getAction() == MotionEvent.ACTION_UP){
+            if(v.getId() == R.id.leftButton || v.getId() == R.id.rightButton | v.getId() == R.id.upButton | v.getId() == R.id.downButton){
+                attemptToSend("" + COMMAND.STOP.name().charAt(0));
+            }
+        }
+        if((event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_UP) && !networkerEstablished){
+            notificationBanner.show();
+        }
+
+        return true;
     }
 
     @Override
@@ -82,47 +116,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.settingsButton:
                 openSettingsMenu();
                 break;
-            case R.id.upButton:
-                if(networkerEstablished){
-                    Log.d("RC-UI", "UP Button Pressed");
-                    attemptToSend("" + COMMAND.UP.name().charAt(0));
-                }else{
-                    notificationBanner.show();
-                }
-                break;
-            case R.id.downButton:
-                if(networkerEstablished){
-                    Log.d("RC-UI", "DOWN Button Pressed");
-                    attemptToSend("" + COMMAND.DOWN.name().charAt(0));
-                }else{
-                    notificationBanner.show();
-                }
-
-                break;
-            case R.id.leftButton:
-                if(networkerEstablished){
-                    Log.d("RC-UI", "LEFT Button Pressed");
-                    attemptToSend("" + COMMAND.LEFT.name().charAt(0));
-                }else {
-                    notificationBanner.show();
-                }
-                break;
-            case R.id.rightButton:
-                if(networkerEstablished){
-                    Log.d("RC-UI", "RIGHT Button Pressed");
-                    attemptToSend("" + COMMAND.RIGHT.name().charAt(0));
-                }else{
-                    notificationBanner.show();
-                }
-                break;
-            case R.id.stopButton:
-                if(networkerEstablished){
-                    Log.d("RC-UI", "STOP Button Pressed");
-                    attemptToSend("" + COMMAND.STOP.name().charAt(0));
-                }else{
-                    notificationBanner.show();
-                }
-
         }
 
     }
@@ -147,14 +140,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void startConnection(){
-        Log.d("RC-UI", "Attempting to Connect");
+        Log.d("RC-NETUI", "Attempting to Connect");
         networker = new Networker(ip);
         if(networker.isConnected()){
             networkerEstablished = true;
             settingsButton.setEnabled(false);
         }else{
             networkerEstablished = false;
-            Log.d("RC-UI", "Failed to Connect");
+            Log.d("RC-NETUI", "Failed to Connect");
             Toast.makeText(getApplicationContext(),"Failed to Connect",Toast.LENGTH_SHORT).show();
         }
 
